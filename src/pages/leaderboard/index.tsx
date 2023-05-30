1f1255083e4d1f205affc082c8e3374c0b2cc7f8
Stalemate99 <sunilkanishq@gmail.com>
2023-05-29 23:41:32 -0400

    feat: leaderboard with top 10 likes implemented

--- src/pages/leaderboard/index.tsx
+++ src/pages/leaderboard/index.tsx
@@ -1,13 +1,69 @@
+import React, { useEffect, useState } from "react";
+import {
+  collection,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
+
 import Navbar from "@/components/Navbar/Navbar";
-import React from "react";
+import QuoteItem from "@/components/Quote/QuoteItem";
+import { firestore } from "@/firebase/config";
 
 type LeaderboardProps = {};
+type QuoteProps = {
+  author_name: string;
+  author_pic: string;
+  likes: number;
+  quote: string;
+  timestamp: Date;
+  id: string;
+};
 
 const Leaderboard: React.FC<LeaderboardProps> = () => {
+  const [quotes, setQuotes] = useState([]);
+  const quotesQuery = query(
+    collection(firestore, "quotes"),
+    orderBy("likes", "desc"),
+    limit(10)
+  );
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(quotesQuery, (querySnapshot) => {
+      const quoteData = [];
+
+      querySnapshot.forEach((quoteDoc) => {
+        quoteData.push({ ...quoteDoc.data(), id: quoteDoc.id });
+      });
+
+      setQuotes(quoteData);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const renderQuotes = () => {
+    return (
+      <>
+        {quotes.map((quote: QuoteProps) => {
+          return <QuoteItem key={quote.id} {...quote} />;
+        })}
+      </>
+    );
+  };
+
   return (
     <main className="relative flexbg-gradient-to-b from-yellow-900 to-yellow-100 h-screen w-full bg-amber-100">
       <Navbar />
-      <h1>Learderboard</h1>
+      <section className="flex flex-col items-center gap-2 m-4">
+        <h2 className="text-amber-900 font-medium text-2xl">Leaderboard</h2>
+        <ul className="flex flex-col gap-4 max-h-[70vh] p-2 w-[90%] lg:w-[50%] pl-4 bg-white rounded-lg items-center overflow-y-scroll">
+          {renderQuotes()}
+        </ul>
+      </section>
     </main>
   );
 };
